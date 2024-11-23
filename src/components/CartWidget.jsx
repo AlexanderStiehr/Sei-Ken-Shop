d7fb410afdd45352f67b371bfafb35951a6f7488
Alexander Stiehr <alex.18@live.com.ar>
2024-11-23 14:11:44 -0300

    v2.3

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -2,7 +2,7 @@
 const CarWidget = ({counter}) => {
     return(
         <div className="carWidgetContainer">
-            <img src="./public/pictures/cart.png" alt="cart" className="cartIcon"/>
+            <img src="../public/pictures/cart.png" alt="cart" className="cartIcon"/>
             <p className="cartCounter">{counter}</p>
         </div>
     )
